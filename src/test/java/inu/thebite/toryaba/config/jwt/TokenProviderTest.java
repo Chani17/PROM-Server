f63f92aecb87db7abc257df664487cb95adf2f05
Chani17 <cucute8878@naver.com>
2023-12-28 14:25:23 +0900

    feat: add TokenProviderTest code

---
+++ src/test/java/inu/thebite/toryaba/config/jwt/TokenProviderTest.java
@@ -0,0 +1,52 @@
+package inu.thebite.toryaba.config.jwt;
+
+import inu.thebite.toryaba.entity.Director;
+import inu.thebite.toryaba.repository.MemberRepository;
+import io.jsonwebtoken.Jwts;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TokenProviderTest {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.secret_key}")
+    private String secret_key;
+
+    /**
+     * generateToken() 검증 테스트
+     * 우선, Director 계정만 가정
+     */
+    @DisplayName("generateToken() : 유저 정보와 만료 기간을 전달해 token을 만들 수 있다.")
+    @Test
+    void generateToken() {
+        // given
+        Director testUser = new Director("test1", "test1", "테스트1", "test1@gmail.com", "010-1234-5678");
+        memberRepository.save(testUser);
+
+        // when
+        String token = tokenProvider.createToken(testUser);
+
+        // then
+        String memberId = Jwts.parser()
+                .setSigningKey(secret_key)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", String.class);
+
+        Assertions.assertThat(memberId).isEqualTo(testUser.getId());
+    }
+    
+}
