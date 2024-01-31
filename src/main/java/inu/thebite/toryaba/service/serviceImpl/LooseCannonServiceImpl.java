c472412942f0674dcaa501dee3b82ca83c3f9c8c
Chani17 <cucute8878@naver.com>
2024-01-31 14:04:33 +0900

    add: LooseCannon controller, service, repository

---
+++ src/main/java/inu/thebite/toryaba/service/serviceImpl/LooseCannonServiceImpl.java
@@ -0,0 +1,10 @@
+package inu.thebite.toryaba.service.serviceImpl;
+
+import inu.thebite.toryaba.service.LooseCannonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LooseCannonServiceImpl implements LooseCannonService {
+}
