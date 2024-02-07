ffea8ac66b21041187ab3818381c6b2ca1531236
Chani17 <cucute8878@naver.com>
2024-02-07 17:19:23 +0900

    add: update teaching board controller api

--- src/main/java/inu/thebite/toryaba/controller/TeachingBoardController.java
+++ src/main/java/inu/thebite/toryaba/controller/TeachingBoardController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "teachingboard")
+@RequestMapping(value = "/teachingboard")
 public class TeachingBoardController {
 
     private final TeachingBoardService teachingBoardService;
