71355fb3bb3ece00c931b81fd1721ed3c85e0b41
Chani17 <cucute8878@naver.com>
2024-02-07 13:44:10 +0900

    feat:  initial teaching board setting

---
+++ src/main/java/inu/thebite/toryaba/controller/TeachingBoardController.java
@@ -0,0 +1,9 @@
+package inu.thebite.toryaba.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TeachingBoardController {
+}
---
+++ src/main/java/inu/thebite/toryaba/service/serviceImpl/TeachingBoardServiceImpl.java
@@ -0,0 +1,10 @@
+package inu.thebite.toryaba.service.serviceImpl;
+
+import inu.thebite.toryaba.service.TeachingBoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TeachingBoardServiceImpl implements TeachingBoardService {
+}
