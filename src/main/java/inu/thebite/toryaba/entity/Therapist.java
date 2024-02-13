c22f1a7e4459309919123b9cafa96b424de1e9a3
Chani17 <cucute8878@naver.com>
2024-02-13 14:56:50 +0900

    add: forte, qualification column in therapist entity

--- src/main/java/inu/thebite/toryaba/entity/Therapist.java
+++ src/main/java/inu/thebite/toryaba/entity/Therapist.java
@@ -16,16 +16,26 @@ import java.time.format.DateTimeFormatter;
 @Getter
 public class Therapist extends Member {
 
+    // 전문 분야
+    @Column(name = "therapist_forte")
+    private String forte;
+
+    // 자격
+    @Column(name = "therapist_qualification")
+    private String qualification;
+
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "center_seq")
     private Center center;
 
-    public Therapist(String id, String password, String name, String email, String phone, Center center) {
+    public Therapist(String id, String password, String name, String email, String phone, String forte, String qualification, Center center) {
         this.id = id;
         this.password = password;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.forte = forte;
+        this.qualification = qualification;
         this.auth = MemberStatus.ROLE_THERAPIST;
         this.approvalYN = "N";
         this.registerDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
