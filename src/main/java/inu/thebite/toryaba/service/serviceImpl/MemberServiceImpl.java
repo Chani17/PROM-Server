bf43ef2c4ac7513343c818b0a9a203cc1b01aca1
Chani17 <cucute8878@naver.com>
2024-02-23 18:22:17 +0900

    Remove: java mail sender

--- src/main/java/inu/thebite/toryaba/service/serviceImpl/MemberServiceImpl.java
+++ src/main/java/inu/thebite/toryaba/service/serviceImpl/MemberServiceImpl.java
@@ -10,9 +10,6 @@ import inu.thebite.toryaba.repository.MemberRepository;
 import inu.thebite.toryaba.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -28,7 +25,7 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final CenterRepository centerRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JavaMailSender javaMailSender;
+//    private final JavaMailSender javaMailSender;
 
 //    @Value("${spring.mail.username}")
 //    private String admin;
